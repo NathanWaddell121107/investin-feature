4a186c980ad426eeac2ad5d556f30d43a1cb54ca
NathanWaddell1211 <waddellnathan1211@gmail.com>
2022-03-08 21:52:15 -0600

    Introduce ant design

--- src/components/ManagedFunds.tsx
+++ src/components/ManagedFunds.tsx
@@ -1,9 +1,49 @@
 import React from "react";
+import { Table } from "antd";
+const columns = [
+  {
+    title: "Platform",
+    dataIndex: "platform",
+    key: "platform",
+  },
+  {
+    title: "Fund Name",
+    dataIndex: "fundName",
+    key: "fundName",
+  },
+  {
+    title: "Fund Performance",
+    dataIndex: "fundPerformance",
+    key: "fundPerformance",
+  },
+  {
+    title: "Value of Your Position",
+    dataIndex: "positionValue",
+    key: "positionValue",
+  },
+];
+// Just test dummy data for now - replace with real data
+const dataSource = [
+  {
+    key: "1",
+    platform: "Investin",
+    fundName: "Some Fund",
+    fundPerformance: "13.24%",
+    positionValue: "$1,450.01",
+  },
+  {
+    key: "2",
+    platform: "Investin",
+    fundName: "Some Other Fund",
+    fundPerformance: "1.04%",
+    positionValue: "$42,066.04",
+  },
+];
 
 export const ManagedFunds: React.FC = () => {
   return (
     <div>
-      <div>managed funds container</div>
+      <Table columns={columns} dataSource={dataSource} />
     </div>
   );
 };
--- src/pages/investin.tsx
+++ src/pages/investin.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { InvestinView } from "../views";
+import "antd/dist/antd.css";
 
 const Investin: NextPage = (props) => {
   return (
